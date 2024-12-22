363ec0ada2d2afbc26d8e104afeb98b82e279e45
Saaket Raman <95548709+SaaketR@users.noreply.github.com>
2024-12-22 06:31:06 +0400

    Update Program.cs

--- WindowsFormsApp/Program.cs
+++ WindowsFormsApp/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace WindowsFormsApp_COP_4365
+namespace WindowsFormsApp
 {
     internal static class Program
     {
