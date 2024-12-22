4a68feb04ead636e4550f7e86fe6d2c5dade4d02
Saaket Raman <95548709+SaaketR@users.noreply.github.com>
2024-12-22 06:35:52 +0400

    Update Recognizer_Bullish_Harami.cs

--- WindowsFormsApp/Recognizer_Bullish_Harami.cs
+++ WindowsFormsApp/Recognizer_Bullish_Harami.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WindowsFormsApp_COP_4365
+namespace WindowsFormsApp
 {
     internal class Recognizer_Bullish_Harami : Recognizer
     {
