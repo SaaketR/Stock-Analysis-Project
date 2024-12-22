76f8a7177a824180034ecdc1e383f7eccae17479
Saaket Raman <95548709+SaaketR@users.noreply.github.com>
2024-12-22 06:40:13 +0400

    Update AssemblyInfo.cs

--- WindowsFormsApp/Properties/AssemblyInfo.cs
+++ WindowsFormsApp/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WindowsFormsApp COP 4365")]
+[assembly: AssemblyTitle("WindowsFormsAp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WindowsFormsApp COP 4365")]
+[assembly: AssemblyProduct("WindowsFormsApp")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
